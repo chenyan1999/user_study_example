ac39267a9a2d7eec91d8bcffc7c0e8017f00454d
Russell <Chenyanliu712@gmail.com>
2023-12-12 21:54:22 +0800

    after

--- Example2/commit_analyzer.py
+++ Example2/commit_analyzer.py
@@ -1,12 +1,15 @@
 import numpy as np
 
-def classify_commit(dataset):
+def classify(dataset, idx):
     dic = {}
     for sample in dataset:
-        commit_id = sample['commit_url'].split('/')[-1]
+        commit_id = sample['commit_url'].split('/')[idx]
         if commit_id not in dic:
             dic[commit_id] = 1
         else:
             dic[commit_id] += 1
     ave = np.mean(list(dic.values()))
     return ave
+
+def classify_commit(dataset):
+    return classify(dataset, -1)
