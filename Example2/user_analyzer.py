d084e89753e702534b5daab21e702babd06d89af
Russell <Chenyanliu712@gmail.com>
2023-12-12 19:02:28 +0800

    after

--- Example2/user_analyzer.py
+++ Example2/user_analyzer.py
@@ -1,12 +1,5 @@
 import numpy as np
+from commit_analyzer import classify
 
 def classify_user(dataset):
-    dic = {}
-    for sample in dataset:
-        user_name = sample['commit_url'].split('/')[-4]
-        if user_name not in dic:
-            dic[user_name] = 1
-        else:
-            dic[user_name] += 1
-    ave = np.mean(list(dic.values()))
-    return ave
+    return classify(dataset, -4)
--- Example3/char.py
+++ Example3/char.py
@@ -1,9 +1,9 @@
 import string
 
-def get_chars():
-  letters = string.ascii_letters
-  digits = string.digits
-  symbols = string.punctuation
+def get_chars():  
+  letters = [c for c in string.ascii_letters]
+  digits = [c for c in string.digits]
+  symbols = [c for c in string.punctuation]
   chars = letters + digits + symbols
   
   return chars
