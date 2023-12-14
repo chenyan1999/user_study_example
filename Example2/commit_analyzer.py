f628f32ab74e9315dafb6868ef3555c5437984c5
Russell <Chenyanliu712@gmail.com>
2023-12-14 14:44:07 +0800

    modify after

--- Example2/commit_analyzer.py
+++ Example2/commit_analyzer.py
@@ -1,12 +1,5 @@
 import numpy as np
+from classify import classify
 
 def classify_commit(dataset):
-    dic = {}
-    for sample in dataset:
-        commit_id = sample['commit_url'].split('/')[-1]
-        if commit_id not in dic:
-            dic[commit_id] = 1
-        else:
-            dic[commit_id] += 1
-    ave = np.mean(list(dic.values()))
-    return ave
+    return classify(dataset, -1)
--- Example2/project_analyzer.py
+++ Example2/project_analyzer.py
@@ -1,12 +1,5 @@
 import numpy as np
+from classify import classify
 
 def classify_project(dataset):
-    dic = {}
-    for sample in dataset:
-        project_name = sample['commit_url'].split('/')[-3]
-        if project_name not in dic:
-            dic[project_name] = 1
-        else:
-            dic[project_name] += 1
-    ave = np.mean(list(dic.values()))
-    return ave
+    return classify(dataset, -3)
